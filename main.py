bd8acc280ab8458a1453884a5e70ee634145d620
theethicalkidd <1688945@acsstudents.com>
2021-01-31 21:32:26 +0000

    Added Listening Status (listening to daddy ethical)

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import random
 from replit import db
 from keep_alive import keep_alive
 
+
+
 client = discord.Client()
+
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -20,6 +25,14 @@ async def on_message(message):
 
     if message.content.startswith('E?neko'):
         await message.channel.send('Command disabled due to lack of cats')
+
+
+# Listening Status
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Daddy Ethical'))
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
 
 
 keep_alive()
